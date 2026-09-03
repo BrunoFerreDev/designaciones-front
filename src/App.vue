57e104a6900ce17eea1677a998edcaeecee01b39
Bruno Ferreira <pittanaferreira@gmail.com>
2026-09-03 02:25:25 -0300

    feat: add @vercel/analytics dependency and integrate analytics component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,57 @@
 <template>
-    <div class="app">
-        <Sidebar v-if="state.isAuthenticated" />
-        <div :class="['main', { 'login-main': !state.isAuthenticated }]">
-            <router-view />
-        </div>
-        <div v-if="state.modal" class="modal-overlay" @click.self="closeModal">
-            <Modal />
-        </div>
+  <div class="app">
+    <Sidebar v-if="state.isAuthenticated" />
+    <div :class="['main', { 'login-main': !state.isAuthenticated }]">
+      <router-view />
     </div>
+    <div v-if="state.modal" class="modal-overlay" @click.self="closeModal">
+      <Modal />
+    </div>
+  </div>
+  <Analytics />
 </template>
 
 <script setup>
 import {
-    state,
-    closeModal,
-    loadArbitros,
-    loadArbitrosNoDisponibles,
-    loadCanchas,
-    loadSuspensiones,
+  state,
+  closeModal,
+  loadArbitros,
+  loadArbitrosNoDisponibles,
+  loadCanchas,
+  loadSuspensiones,
 } from "./store";
 import Sidebar from "./components/Sidebar.vue";
 import Modal from "./components/Modal.vue";
 import { onMounted, watch } from "vue";
-
+import { Analytics } from "@vercel/analytics/vue";
 const loadData = () => {
-    if (state.isAuthenticated) {
-        loadArbitros();
-        loadArbitrosNoDisponibles();
-        loadCanchas();
-        loadSuspensiones();
-    }
+  if (state.isAuthenticated) {
+    loadArbitros();
+    loadArbitrosNoDisponibles();
+    loadCanchas();
+    loadSuspensiones();
+  }
 };
 
 onMounted(() => {
-    loadData();
+  loadData();
 });
 
 watch(
-    () => state.isAuthenticated,
-    (newVal) => {
-        if (newVal) {
-            loadData();
-        }
-    },
+  () => state.isAuthenticated,
+  (newVal) => {
+    if (newVal) {
+      loadData();
+    }
+  },
 );
 </script>
 
 <style>
 .login-main {
-    margin-left: 0 !important;
-    width: 100% !important;
-    padding-bottom: 0 !important;
-    height: 100vh;
+  margin-left: 0 !important;
+  width: 100% !important;
+  padding-bottom: 0 !important;
+  height: 100vh;
 }
 </style>
